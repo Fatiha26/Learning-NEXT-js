0353f36a1782b42775a47600f4060bf5f23915bd
Fatiha26 <f6lamia@gmail.com>
2024-08-20 21:14:35 +0600

    SC VS CC

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,11 @@
+
 import React from 'react'
 
 export default function About() {
   return (
     <main className="mt-10">
         <div>This is About page!</div>
+       
     </main>
   )
 }
--- app/about/vision/page.js
+++ app/about/vision/page.js
@@ -1,9 +1,15 @@
+"use client"
 import React from 'react'
 
 export default function About() {
   return (
     <main className="mt-10">
         <div>This is Vision page!</div>
+        <button className='bg-green-500
+            rounded-sm px-4 py-1'
+            onClick={()=> console.log("clicked")}>
+          Click Here
+        </button>
     </main>
   )
 }
